66ad44cfaad3a5e74107fa10a5688930140fe142
ColeThatGuy <143652517+ColeThatGuy@users.noreply.github.com>
2023-09-08 22:39:06 -0400

    new weew
    walter debug

--- src/title.gd
+++ src/title.gd
@@ -7,9 +7,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var label = get_node("text_turtle")
-	label.position.y -= 1;
+	var label = get_node('text_turtle')
+	label.position.y -= 0.07
+	var start = get_node('Start')
+	if start.position.y >= 95:
+		start.position.y -= 0.07
+	var player = get_node("Sprite2D")
+	var walt = get_node("Walter")
+	walt.position.y -= 0.07
 	
-	
+
+
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://src/level.tscn")
+	get_tree().change_scene_to_file("res://src/turtle.tscn")
