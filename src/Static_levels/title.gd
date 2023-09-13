5a709b09536f2dc66d8f90576a142924b0bc782c
John Patch <legojrp@gmail.com>
2023-09-12 21:56:45 -0400

    done frfr no cap

--- src/Static_levels/title.gd
+++ src/Static_levels/title.gd
@@ -1,5 +1,5 @@
 extends Node2D
-var speed = 0.09
+var speed1 = 0.09
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,17 +8,18 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var label = get_node('text_turtle')
-	label.position.y -= speed
+	var speed2 = speed1 * delta * 50
+	label.position.y -= speed2
 	var start = get_node('Start')
 	if start.position.y >= 95:
-		start.position.y -= speed
+		start.position.y -= speed2
 	var walt = get_node("Walter")
-	walt.position.y -= speed
+	walt.position.y -= speed2
 	
 	if Input.is_action_pressed("debug") && Input.is_action_pressed("debug2"):
-		speed = 20
+		speed1 = 20
 	else:
-		speed = 0.09
+		speed1 = 0.09
 
 
 func _on_start_pressed():
